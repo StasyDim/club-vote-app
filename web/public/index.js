1eb0a5fd98ab56f2a6d8a3f74a328659075a1d95
StasyDim <sramejlivkata@abv.bg>
2026-03-23 11:14:45 +0200

    Добавих CreatePollScreen с динамични полета

---
+++ web/public/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <h1>Club Vote App</h1>
+  </React.StrictMode>
+);
